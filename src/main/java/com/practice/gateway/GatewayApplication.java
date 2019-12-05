75af85c3e330b6cc3c04efcc48a16b84ff9ca415
최준우 <junwoochoi@zuminternet.com>
2019-12-05 15:28:52 +0900

    feat : add route '/headers'

--- src/main/java/com/practice/gateway/GatewayApplication.java
+++ src/main/java/com/practice/gateway/GatewayApplication.java
@@ -2,12 +2,24 @@ package com.practice.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
 
+    @Bean
+    public RouteLocator myRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("path", r -> r.path("/headers")
+                        .filters(f -> f.addRequestHeader("X-Foo", "Bar"))
+                        .uri("http://httpbin.org")
+                )
+                .build();
+    }
 }
